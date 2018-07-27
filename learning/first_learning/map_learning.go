d4be752abd564e3497e89c050663cb2ce897988f
zhangxinyue <zhangxinyue@uotc.com>
2018-07-27 14:38:11 +0800

    modify

--- learning/first_learning/map_learning.go
+++ learning/first_learning/map_learning.go
@@ -6,6 +6,12 @@ import (
 )
 
 func main()  {
+
+	//SortTest()
+	MapHasValue()
+}
+
+func MakeMap1(){
 	m := make(map[int]string)
 	fmt.Println(m)
 	m[1] = "ok"
@@ -14,17 +20,15 @@ func main()  {
 	fmt.Println(m)
 	a := m[3]
 	fmt.Println(a)
+}
 
-
+func MakeMap2(){
 	m1 := make(map[int]map[int]string)
 	m1[1] = make(map[int]string)
 	m1[1][1] = "ok"
 	fmt.Println(m1)
 	//b, ok := m1[2][1]
 	//fmt.Println(b, ok)
-
-
-	SortTest()
 }
 
 
@@ -42,5 +46,16 @@ func SortTest()(){
 
 	for _, key := range keys {
 		fmt.Println("Key:", key, "Value:", a[key])
+	}
+}
+
+func MapHasValue(){
+	a := map[string]interface{}{}
+	a["a_test"] = 1
+	a["m_test"] = "hello"
+	a["b_test"] = 12.34
+
+	if val, hasValue := a["c_test"]; hasValue {
+		fmt.Println(val)
 	}
 }
